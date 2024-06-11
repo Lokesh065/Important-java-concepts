703aeb40d4561f0069419dbd57824322ee088506
Lokesh065 <lokeshlohitha764@gmail.com>
2024-06-11 10:19:52 +0530

    Add files via upload
    String class

---
+++ StringSubclass.java
@@ -0,0 +1,12 @@
+//to demonstrate the error
+
+//can't inherit final classes
+package stringclass;
+
+public class StringSubclass extends SampleStringDemo {
+
+	public static void main(String[] args) {
+		
+
+	}
+}
