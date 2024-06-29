e9877bf9046fb2d88b193d56d769c7b1265fc94e
lokesh <lokesh@LAPTOP-B6SAJ78U>
2024-06-29 05:34:50 +0530

    Functional interfaces

---
+++ JavaBobber/src/Funinterfaceex/MyCube.java
@@ -0,0 +1,8 @@
+//Define Functional Interface MyCube
+package Funinterfaceex;
+
+@FunctionalInterface
+public interface MyCube {
+	int getCube(int no);
+	//void show(); not allow more than one abstract method
+}
---
+++ JavaBobber/src/Funinterfaceex/Operations.java
@@ -0,0 +1,8 @@
+//Define FunctionalInterface Operations
+
+package Funinterfaceex;
+
+@FunctionalInterface
+public interface Operations {
+	float performArithmetic(int a, int b);
+}
